90f2ab483b747a52f02e1fc2b791038ff3a27934
kota-shiokara <ikanoshiokara.fun@gmail.com>
2024-05-08 03:58:26 +0900

    react start

--- web/next-app/front/src/app/page.tsx
+++ web/next-app/front/src/app/page.tsx
@@ -1,17 +1,45 @@
-export default function Home() {
-  return (
-    <main>
-      <ColorMessage color="red">
-        <div>元気です！</div>
-      </ColorMessage>
-    </main>
-  );
+'use client'
+import { useState } from "react";
+
+type Child1Props = {
+  children: React.ReactNode;
+};
+function Child1({children}: Child1Props) {
+  console.log(`Child1`);
+  return <div>{children}</div>;
 }
 
-export const ColorMessage = (props: any) => {
-  const contentStyle = {
-    color: props.color,
-    fontSize: "24px"
+function Child2() {
+  console.log("Child2");
+  return <div>Child2</div>;
+}
+
+function Child3() {
+  console.log("Child3");
+  return <div>Child3</div>;
+}
+
+function Child4() {
+  console.log("Child4");
+  return <div>Child4</div>;
+}
+
+export default function Home() {
+  const [num, setNum] = useState(0);
+
+  const onClick = () => {
+    setNum(num + 1);
   }
-  return <div style={contentStyle}>{props.children}</div>
+
+  return (
+    <main>
+      <Child1>
+        <Child2 />
+        <Child3 />
+      </Child1>
+      <Child4 />
+      <button onClick={onClick}>Click</button>
+      <p>{num}</p>
+    </main>
+  );
 }
